b7b26165df5e19f5ae8331c51cd261d9e888a776
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-02-14 21:02:05 +0100

    feat(86c21g3xx): add members module to application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { GuildModule } from './guilds/guilds.module';
 import { CampusModule } from './campuses/campuses.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     GuildModule,
     CampusModule,
+    MembersModule,
   ],
   controllers: [],
   providers: [],

0a7580492929b4bf07f7e05069f6db09d3230ba2
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-24 20:58:55 +0100

    refactor(votes): update module imports and dependencies

--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VotesService } from './votes.service';
 import { VotesController } from './votes.controller';
 import { Vote } from './entities/vote.entity';
+import { Member } from '../members/entities/member.entity';
+import { Resource } from '../resources/entities/resource.entity';
+import { Comment } from '../comments/entities/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vote])],
+  imports: [TypeOrmModule.forFeature([Vote, Member, Resource, Comment])],
   controllers: [VotesController],
   providers: [VotesService],
   exports: [VotesService]

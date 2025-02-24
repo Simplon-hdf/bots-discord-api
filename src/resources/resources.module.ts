910a77c67ff0633b405779a894b73b11a388ed63
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-24 20:01:25 +0100

    feat(resources): add Comment entity to module imports

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -4,9 +4,10 @@ import { ResourcesService } from './resources.service';
 import { ResourcesController } from './resources.controller';
 import { Resource } from './entities/resource.entity';
 import { Member } from '../members/entities/member.entity';
+import { Comment } from '../comments/entities/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resource, Member])],
+  imports: [TypeOrmModule.forFeature([Resource, Member, Comment])],
   controllers: [ResourcesController],
   providers: [ResourcesService],
   exports: [ResourcesService],

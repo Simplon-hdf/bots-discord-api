c9acb5c5a715f633969ebf99f15b54086fb0e1c8
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-24 16:45:03 +0100

    fix(roles): update update member roles DTO

--- src/roles/dto/update-member-roles.dto.ts
+++ src/roles/dto/update-member-roles.dto.ts
@@ -2,5 +2,5 @@ import { OmitType } from "@nestjs/swagger";
 import { AssignRoleToMemberDto } from "./assign-role-to-member.dto";
 
 export class UpdateMemberRolesDto extends OmitType(AssignRoleToMemberDto, [
-    'uuid_member'] as const) {}
+    'uuidMember'] as const) {}
 

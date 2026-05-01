1858b337a5bd646b5e56a30c3018f7d4ec82315b
Aurore Reynier <reynier.aurore@gmail.com>
2026-05-01 20:53:24 +0200

    refactor(86c9kt17t): implement interface for guilds-templates module

--- src/guilds-templates/guilds-templates.controller.ts
+++ src/guilds-templates/guilds-templates.controller.ts
@@ -1,19 +1,44 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException } from '@nestjs/common';
-import { GuildsTemplatesService } from './guilds-templates.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  NotFoundException,
+  Inject,
+} from '@nestjs/common';
 import { CreateGuildTemplateDto } from './dto/create-guild-template.dto';
 import { UpdateGuildTemplateDto } from './dto/update-guild-template.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { GuildTemplate } from './entities/guild-template.entity';
+import {
+  IGuildsTemplatesService,
+  IGuildsTemplatesServiceToken,
+} from './interfaces/guild-template.interface';
 
 @ApiTags('guilds-templates')
 @ApiBearerAuth('JWT-auth')
 @Controller('guilds-templates')
 export class GuildsTemplatesController {
-  constructor(private readonly guildsTemplatesService: GuildsTemplatesService) {}
+  constructor(
+    @Inject(IGuildsTemplatesServiceToken)
+    private readonly guildsTemplatesService: IGuildsTemplatesService,
+  ) {}
 
   @Post()
   @ApiOperation({ summary: 'Créer un nouveau template de serveur' })
-  @ApiResponse({ status: 201, description: 'Le template a été créé avec succès.', type: GuildTemplate })
+  @ApiResponse({
+    status: 201,
+    description: 'Le template a été créé avec succès.',
+    type: GuildTemplate,
+  })
   @ApiResponse({ status: 400, description: 'Requête invalide' })
   create(@Body() createGuildTemplateDto: CreateGuildTemplateDto) {
     return this.guildsTemplatesService.create(createGuildTemplateDto);
@@ -21,14 +46,22 @@ export class GuildsTemplatesController {
 
   @Get()
   @ApiOperation({ summary: 'Récupérer tous les templates de serveur' })
-  @ApiResponse({ status: 200, description: 'Liste des templates récupérée avec succès.', type: [GuildTemplate] })
+  @ApiResponse({
+    status: 200,
+    description: 'Liste des templates récupérée avec succès.',
+    type: [GuildTemplate],
+  })
   findAll() {
     return this.guildsTemplatesService.findAll();
   }
 
   @Get(':uuid')
   @ApiOperation({ summary: 'Récupérer un template de serveur par son UUID' })
-  @ApiResponse({ status: 200, description: 'Le template a été trouvé.', type: GuildTemplate })
+  @ApiResponse({
+    status: 200,
+    description: 'Le template a été trouvé.',
+    type: GuildTemplate,
+  })
   @ApiResponse({ status: 404, description: 'Template non trouvé' })
   findOne(@Param('uuid') uuid: string) {
     return this.guildsTemplatesService.findOne(uuid);
@@ -36,19 +69,34 @@ export class GuildsTemplatesController {
 
   @Put(':uuid')
   @ApiOperation({ summary: 'Mettre à jour un template de serveur' })
-  @ApiResponse({ status: 200, description: 'Le template a été mis à jour avec succès.', type: GuildTemplate })
+  @ApiResponse({
+    status: 200,
+    description: 'Le template a été mis à jour avec succès.',
+    type: GuildTemplate,
+  })
   @ApiResponse({ status: 404, description: 'Template non trouvé' })
-  async update(@Param('uuid') uuid: string, @Body() updateGuildTemplateDto: UpdateGuildTemplateDto) {
-    const guildTemplate = await this.guildsTemplatesService.update(uuid, updateGuildTemplateDto);
+  async update(
+    @Param('uuid') uuid: string,
+    @Body() updateGuildTemplateDto: UpdateGuildTemplateDto,
+  ) {
+    const guildTemplate = await this.guildsTemplatesService.update(
+      uuid,
+      updateGuildTemplateDto,
+    );
     if (!guildTemplate) {
-      throw new NotFoundException(`Guild template with UUID "${uuid}" not found`);
+      throw new NotFoundException(
+        `Guild template with UUID "${uuid}" not found`,
+      );
     }
     return guildTemplate;
   }
 
   @Delete(':uuid')
   @ApiOperation({ summary: 'Supprimer un template de serveur' })
-  @ApiResponse({ status: 200, description: 'Le template a été supprimé avec succès.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Le template a été supprimé avec succès.',
+  })
   @ApiResponse({ status: 404, description: 'Template non trouvé' })
   remove(@Param('uuid') uuid: string) {
     return this.guildsTemplatesService.remove(uuid);
--- src/guilds-templates/guilds-templates.module.ts
+++ src/guilds-templates/guilds-templates.module.ts
@@ -3,10 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GuildsTemplatesService } from './guilds-templates.service';
 import { GuildsTemplatesController } from './guilds-templates.controller';
 import { GuildTemplate } from './entities/guild-template.entity';
+import { IGuildsTemplatesServiceToken } from './interfaces/guild-template.interface';
 
 @Module({
   imports: [TypeOrmModule.forFeature([GuildTemplate])],
   controllers: [GuildsTemplatesController],
-  providers: [GuildsTemplatesService],
+  providers: [
+    {
+      provide: IGuildsTemplatesServiceToken,
+      useClass: GuildsTemplatesService,
+    },
+  ],
+  exports: [IGuildsTemplatesServiceToken],
 })
 export class GuildsTemplatesModule {}
